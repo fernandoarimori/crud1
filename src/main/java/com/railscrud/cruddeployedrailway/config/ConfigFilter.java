68ebeaa1a9e20243a5d72dc3ce4346203080da61
Fernando Castilho Arimori <49201289+fernandoarimori@users.noreply.github.com>
2024-01-05 17:06:27 -0300

    Filter Config v3

--- src/main/java/com/railscrud/cruddeployedrailway/config/ConfigFilter.java
+++ src/main/java/com/railscrud/cruddeployedrailway/config/ConfigFilter.java
@@ -3,7 +3,6 @@ package com.railscrud.cruddeployedrailway.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -11,11 +10,21 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class ConfigFilter{
+public class ConfigFilter {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.authorizeHttpRequests(
-                request->request.requestMatchers("/swagger-ui.html").permitAll().anyRequest().authenticated()
-        ).build();
+        return httpSecurity.cors((cors) -> cors.disable())
+                .csrf((csrf) -> csrf.disable())
+                .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(
+                        (request) -> request.requestMatchers(HttpMethod.GET, "/**")
+                                .permitAll()
+                                .requestMatchers(HttpMethod.POST, "/user").permitAll()
+                                .requestMatchers(HttpMethod.PUT, "/user").permitAll()
+                                .requestMatchers(HttpMethod.DELETE, "/user/**").permitAll()
+                                .requestMatchers("/swagger-ui.html","/v3/api-docs/**","/swagger-ui/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .build();
     }
 }
