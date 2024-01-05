69ecea4a5de2304c6923b507b324d7aac02588ed
Fernando Castilho Arimori <49201289+fernandoarimori@users.noreply.github.com>
2024-01-05 15:49:28 -0300

    add:spring security, filter config

---
+++ src/main/java/com/railscrud/cruddeployedrailway/config/ConfigFilter.java
@@ -0,0 +1,21 @@
+package com.railscrud.cruddeployedrailway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class ConfigFilter{
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.authorizeHttpRequests(
+                request->request.requestMatchers("/*").permitAll()
+        ).build();
+    }
+}
